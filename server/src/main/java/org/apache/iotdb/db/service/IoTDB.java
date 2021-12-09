19271da22970993ff12144434638948a5bb093e1
chengjianyun <chengjianyun@360.cn>
2021-12-09 11:14:35 +0800

    remove unnecessary procedure in start in IoTDB

--- server/src/main/java/org/apache/iotdb/db/service/IoTDB.java
+++ server/src/main/java/org/apache/iotdb/db/service/IoTDB.java
@@ -23,28 +23,12 @@ import org.apache.iotdb.db.conf.IoTDBConfigCheck;
 import org.apache.iotdb.db.conf.IoTDBConstant;
 import org.apache.iotdb.db.conf.IoTDBDescriptor;
 import org.apache.iotdb.db.conf.rest.IoTDBRestServiceCheck;
-import org.apache.iotdb.db.conf.rest.IoTDBRestServiceDescriptor;
-import org.apache.iotdb.db.cost.statistic.Measurement;
-import org.apache.iotdb.db.cq.ContinuousQueryService;
 import org.apache.iotdb.db.engine.StorageEngine;
-import org.apache.iotdb.db.engine.cache.CacheHitRatioMonitor;
-import org.apache.iotdb.db.engine.compaction.CompactionTaskManager;
-import org.apache.iotdb.db.engine.compaction.cross.inplace.manage.MergeManager;
-import org.apache.iotdb.db.engine.flush.FlushManager;
-import org.apache.iotdb.db.engine.trigger.service.TriggerRegistrationService;
 import org.apache.iotdb.db.exception.ConfigurationException;
 import org.apache.iotdb.db.exception.StartupException;
 import org.apache.iotdb.db.metadata.MManager;
-import org.apache.iotdb.db.monitor.StatMonitor;
-import org.apache.iotdb.db.query.udf.service.TemporaryQueryDataFileService;
-import org.apache.iotdb.db.query.udf.service.UDFClassLoaderManager;
-import org.apache.iotdb.db.query.udf.service.UDFRegistrationService;
 import org.apache.iotdb.db.rescon.PrimitiveArrayManager;
 import org.apache.iotdb.db.rescon.SystemInfo;
-import org.apache.iotdb.db.rescon.TVListAllocator;
-import org.apache.iotdb.db.rest.RestService;
-import org.apache.iotdb.db.sync.receiver.SyncServerManager;
-import org.apache.iotdb.db.writelog.manager.MultiFileLogNodeManager;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -116,40 +100,40 @@ public class IoTDB implements IoTDBMBean {
     setUncaughtExceptionHandler();
     logger.info("recover the schema...");
     initMManager();
-    registerManager.register(JMXService.getInstance());
-    registerManager.register(FlushManager.getInstance());
-    registerManager.register(MultiFileLogNodeManager.getInstance());
-    registerManager.register(Measurement.INSTANCE);
-    registerManager.register(TVListAllocator.getInstance());
-    registerManager.register(CacheHitRatioMonitor.getInstance());
-    registerManager.register(MergeManager.getINSTANCE());
-    registerManager.register(CompactionTaskManager.getInstance());
-    JMXService.registerMBean(getInstance(), mbeanName);
-    registerManager.register(StorageEngine.getInstance());
-    registerManager.register(TemporaryQueryDataFileService.getInstance());
-    registerManager.register(UDFClassLoaderManager.getInstance());
-    registerManager.register(UDFRegistrationService.getInstance());
-    registerManager.register(TriggerRegistrationService.getInstance());
-    registerManager.register(ContinuousQueryService.getInstance());
+    //    registerManager.register(JMXService.getInstance());
+    //    registerManager.register(FlushManager.getInstance());
+    //    registerManager.register(MultiFileLogNodeManager.getInstance());
+    //    registerManager.register(Measurement.INSTANCE);
+    //    registerManager.register(TVListAllocator.getInstance());
+    //    registerManager.register(CacheHitRatioMonitor.getInstance());
+    //    registerManager.register(MergeManager.getINSTANCE());
+    //    registerManager.register(CompactionTaskManager.getInstance());
+    //    JMXService.registerMBean(getInstance(), mbeanName);
+    //    registerManager.register(StorageEngine.getInstance());
+    //    registerManager.register(TemporaryQueryDataFileService.getInstance());
+    //    registerManager.register(UDFClassLoaderManager.getInstance());
+    //    registerManager.register(UDFRegistrationService.getInstance());
+    //    registerManager.register(TriggerRegistrationService.getInstance());
+    //    registerManager.register(ContinuousQueryService.getInstance());
 
     // in cluster mode, RPC service is not enabled.
-    if (IoTDBDescriptor.getInstance().getConfig().isEnableRpcService()) {
-      registerManager.register(RPCService.getInstance());
-    }
-
-    if (IoTDBDescriptor.getInstance().getConfig().isEnableInfluxDBRpcService()) {
-      registerManager.register(InfluxDBRPCService.getInstance());
-    }
-
-    if (IoTDBDescriptor.getInstance().getConfig().isEnableMetricService()) {
-      registerManager.register(MetricsService.getInstance());
-    }
-    if (IoTDBDescriptor.getInstance().getConfig().isEnableMQTTService()) {
-      registerManager.register(MQTTService.getInstance());
-    }
-    if (IoTDBRestServiceDescriptor.getInstance().getConfig().isEnableRestService()) {
-      registerManager.register(RestService.getInstance());
-    }
+    //    if (IoTDBDescriptor.getInstance().getConfig().isEnableRpcService()) {
+    //      registerManager.register(RPCService.getInstance());
+    //    }
+    //
+    //    if (IoTDBDescriptor.getInstance().getConfig().isEnableInfluxDBRpcService()) {
+    //      registerManager.register(InfluxDBRPCService.getInstance());
+    //    }
+    //
+    //    if (IoTDBDescriptor.getInstance().getConfig().isEnableMetricService()) {
+    //      registerManager.register(MetricsService.getInstance());
+    //    }
+    //    if (IoTDBDescriptor.getInstance().getConfig().isEnableMQTTService()) {
+    //      registerManager.register(MQTTService.getInstance());
+    //    }
+    //    if (IoTDBRestServiceDescriptor.getInstance().getConfig().isEnableRestService()) {
+    //      registerManager.register(RestService.getInstance());
+    //    }
     logger.info("IoTDB is set up, now may some sgs are not ready, please wait several seconds...");
 
     while (!StorageEngine.getInstance().isAllSgReady()) {
@@ -163,10 +147,10 @@ public class IoTDB implements IoTDBMBean {
     }
 
     // Warn: registMonitor() method should be called after systemDataRecovery()
-    registerManager.register(StatMonitor.getInstance());
-    registerManager.register(SyncServerManager.getInstance());
-    registerManager.register(UpgradeSevice.getINSTANCE());
-    registerManager.register(SettleService.getINSTANCE());
+    //    registerManager.register(StatMonitor.getInstance());
+    //    registerManager.register(SyncServerManager.getInstance());
+    //    registerManager.register(UpgradeSevice.getINSTANCE());
+    //    registerManager.register(SettleService.getINSTANCE());
 
     logger.info("Congratulation, IoTDB is set up successfully. Now, enjoy yourself!");
   }
